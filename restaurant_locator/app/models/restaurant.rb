31d3a196693f30e3d616606dd9fcfaf2917c1bfd
mpragata <mapragata@gbox.adnu.edu.ph>
2017-03-10 23:07:17 +0800

    temporary validator(resto registration)

--- restaurant_locator/app/models/restaurant.rb
+++ restaurant_locator/app/models/restaurant.rb
@@ -1,4 +1,17 @@
 class Restaurant < ActiveRecord::Base
 	has_many :menutypes
 	has_many :reviews
+	
+	# Restaurant Validators (Temp)
+	validates :r_username, presence: true, uniqueness: true
+	validates :r_password, presence: true, length: {minimum: 8, maximum: 12}
+	validates :restaurant_name, presence: true, uniqueness: true
+	validates :r_contact, presence: true, length: {minimum: 12, maximum: 12}
+	validates :r_address, presence: true
+	validates :r_style, presence: true
+	validates :r_email, presence: true # need to have regex format, d ko pa alam
+	validates :r_start_time, presence: true, format: {with: /[0-9][0-9]:[0-9][0-9]/} # brute force format
+	validates :r_close_time, presence: true, format: {with: /[0-9][0-9]:[0-9][0-9]/} # brute force format
+	validates :r_longitude, presence: true # need to select a place
+	validates :r_latitude, presence: true # need to select a place
 end
